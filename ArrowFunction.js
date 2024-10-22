c5c3afd6a0166d3963f78f70c19cff73cc10fd60
kishan kumar jaiswal <123975775+kishan2805@users.noreply.github.com>
2024-10-22 15:35:45 +0530

    Add files via upload

---
+++ ArrowFunction.js
@@ -0,0 +1,18 @@
+// arrow fun with 1 arg
+const squ=num=>num*num;
+console.log(squ(3))
+
+// arror function with2  arg
+const sum=(a,b)=>a+b
+console.log(sum(2,3))
+
+// arrow fun with multiple line s
+const newwfun=(a,b)=>{
+    const multi=a*b;
+    return multi
+};
+console.log(newwfun(2,8));
+
+// arrow function with 0 arg
+const printing=()=>"hello hello"
+console.log(printing())
